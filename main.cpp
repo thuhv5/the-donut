bbf2c7082d720bf066898f7757d8ba9eea4e738c
thuhv5 <hoangvanthufl@gmail.com>
2025-06-29 22:49:03 +0700

    Hình vòng, tĩnh

--- main.cpp
+++ main.cpp
@@ -7,10 +7,11 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
     //the terminal is 80x24, the pixel is .,-~:;=!*#$@ from dimmest to brightest
     // now build the static simple plate with @
-    for (int i = 0; i < 24; ++i)
+    for (int i = 0; i < 25; ++i)
     {
         for (int j = 0; j < 80; ++j)
-            if ((12-i)*(12-i) + (12-j)*(12-j) <= 12*12)
+            if ((12-i)*(12-i) + (12-j)*(12-j) <= 12*12
+                    && (12-i)*(12-i) + (12-j)*(12-j) >= 11*11)
                 cout << "@@";
             else
                 cout << "  ";
