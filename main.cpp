84dd520468ab8a50add78662ded14c3f37409c60
thuhv5 <hoangvanthufl@gmail.com>
2025-06-29 10:48:35 +0700

    Cái đĩa, nhìn hơi ngu nhưng mà cũng ok

--- main.cpp
+++ main.cpp
@@ -5,8 +5,17 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
-    cout << "hello" << endl;
+    //the terminal is 80x24, the pixel is .,-~:;=!*#$@ from dimmest to brightest
+    // now build the static simple plate with @
+    for (int i = 0; i < 24; ++i)
+    {
+        for (int j = 0; j < 80; ++j)
+            if ((12-i)*(12-i) + (12-j)*(12-j) <= 12*12)
+                cout << "@@";
+            else
+                cout << "  ";
+        cout << endl;
+    }
 
     return a.exec();
 }
